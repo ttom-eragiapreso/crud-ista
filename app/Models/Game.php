691f159e64af8d95633c2bb3b8ec88adaec55d64
carmelopalumbo <carmelopalumbo@outlook.it>
2023-01-16 17:20:14 +0100

    add-create

--- app/Models/Game.php
+++ app/Models/Game.php
@@ -9,6 +9,7 @@ use Illuminate\Database\Eloquent\Model;
 class Game extends Model
 {
     use HasFactory;
+    protected $fillable = ['name', 'genres', 'developers', 'publishers', 'release'];
 
     public static function generateSlug($string)
     {
--- resources/views/games/create.blade.php
+++ resources/views/games/create.blade.php
@@ -1,5 +1,31 @@
 @extends('layout.main')
 
 @section('content')
-  <h1>Create</h1>
+    <h1 class="text-center py-5">AGGIUNGI UN NUOVO GIOCO</h1>
+
+    <form action="{{ route('games.store') }}" method="POST" class="w-50 m-auto">
+        @csrf
+        <div class="mb-3">
+            <label for="name" class="form-label">Nome Gioco</label>
+            <input type="text" class="form-control" name="name" id="name" placeholder="New game...">
+        </div>
+        <div class="mb-3">
+            <label for="genres" class="form-label">Generi</label>
+            <input type="text" class="form-control" name="genres" id="genres" placeholder="Genres here...">
+        </div>
+        <div class="mb-3">
+            <label for="developers" class="form-label">Sviluppatori</label>
+            <input type="text" class="form-control" name="developers" id="developers" placeholder="Developers here...">
+        </div>
+        <div class="mb-3">
+            <label for="publishers" class="form-label">Publishers</label>
+            <input type="text" class="form-control" name="publishers" id="publishers" placeholder="Publishers here...">
+        </div>
+        <div class="mb-3">
+            <label for="release" class="form-label">Data di uscita</label>
+            <input type="text" class="form-control" name="release" id="release" placeholder="Release date here...">
+        </div>
+
+        <button class="btn btn-success d-block w-50 m-auto my-5" type="submit">AGGIUNGI</button>
+    </form>
 @endsection
