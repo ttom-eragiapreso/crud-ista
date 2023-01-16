9ab7afca57777ee2cc807a58c294e58ba84c3fe6
mlietti1 <mcf.lietti@outlook.com>
2023-01-16 17:16:05 +0100

    show view

--- resources/views/games/show.blade.php
+++ resources/views/games/show.blade.php
@@ -1,5 +1,14 @@
 @extends('layout.main')
 
 @section('content')
-  <h1>Show</h1>
+
+    <div class="container">
+        <h1 class="mb-3">{{$game->name}}</h1>
+        <a class="btn btn-warning" href="{{route('games.edit', $game)}}">Edit Game</a>
+        <p class="my-3"><strong>Genres: </strong>{{$game->genres}}</p>
+        <p class="mb-3"><strong>Developers: </strong>{{$game->developers}}</p>
+        <p class="mb-3"><strong>Publishers: </strong>{{$game->publishers}}</p>
+        <p><strong>Release: </strong>{{$game->release}}</p>
+    </div>
+
 @endsection
